a9c59b72d49ed8eb3039246ef51bd8b34746442b
J1-LOL <74592729+J1-LOL@users.noreply.github.com>
2020-12-28 15:33:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,109 @@
+canvas = document.getElementById("canvas");
+ctx = canvas.getContext("2d");
+
+car1_width = 120;
+car1_height = 70;
+car1_image = "car 1.png";
+car1_x = 10;
+car1_y = 10;
+
+car2_width = 120;
+car2_height = 70;
+car2_image = "car 1.png";
+car2_x = 10;
+car2_y = 10;
+
+background_image = "finish.png"
+
+function add(){
+    background_imgTag = new Image();
+    background_imgTag.onlad = uploadBackground;
+    background_imgTag.src = background_image;
+
+    car1_imgTag = new  Image();
+    car1_imgTag.onlad = uploadCar1;
+    car1_imgTag.src = car1_image;
+
+    car2_imgTag = new  Image();
+    car2_imgTag.onlad = uploadCar2;
+    car2_imgTag.src = car2_image;
+}
+
+function uploadBackground(){
+    ctx.drawImage(background_imgTag, 0, 0, canvas.width, cavas.height);
+}
+
+function uploadCar1(){
+    ctx.drawImage(car1_imgTag, 0, 0, canvas.width, cavas.height);
+}
+
+function uploadCar2(){
+    ctx.drawImage(car2_imgTag, 0, 0, canvas.width, cavas.height);
+}
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e){
+    keyPressesed = e.keyCode;
+    console.log(keyPressesed);
+
+    if (keyPressesed == 38){
+        car1_up();
+        console.log("up arrow key");
+    }
+
+    if (keyPressesed == 40){
+        car1_down();
+        console.log("down arrow key");
+    }
+
+    if (keyPressesed == 39){
+        car1_right();
+        console.log("right arrow key");
+    }
+
+    if (keyPressesed == 37){
+        car1_left();
+        console.log("left arrow key");
+    }
+
+    if (keyPressesed == 87){
+        car2_up();
+        console.log("key w");
+    }
+
+    if (keyPressesed == 83){
+        car2_down();
+        console.log("key s");
+    }
+
+    if (keyPressesed == 68){
+        car2_right();
+        console.log("key d");
+    }
+
+    if (keyPressesed == 87){
+        car2_left();
+        console.log("key a");
+    }
+}
+
+function car1_up(){
+    if (car1_y >= 0){
+        car1_y = car1_y - 10;
+        comsole.log("When up arrow is pressed, x = " + car1_x + " | y = " + car1_y);
+        uploadBackground();
+        uploadCar1();
+        uploadCar2();
+    }
+}
+
+function car1_down(){
+    if (car1_y <= 500){
+        car1_y = car1_y + 10;
+        comsole.log("When down arrow is pressed, x = " + car1_x + " | y = " + car1_y);
+        uploadBackground();
+        uploadCar1();
+        uploadCar2();
+    }
+}
